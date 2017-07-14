279b288d5ef74248f5447fa2a2485ba31d8bc942
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-07-15 04:20:07 +0900

    QA (#1)
    * QA
    
    * make sure server restarts reliably
    
    when restarting, there is a race and starting server fails
    
    * QA

--- tests/serverspec/client_only_spec.rb
+++ tests/serverspec/client_only_spec.rb
@@ -1,34 +1,13 @@
 require "spec_helper"
 require "serverspec"
 
-package = "mosquitto"
 service = "mosquitto"
-config  = "/etc/mosquitto/mosquitto.conf"
-user    = "mosquitto"
-group   = "mosquitto"
-ports   = [ 1883 ]
-db_dir  = "/var/lib/mosquitto"
-default_user = "root"
-default_group = "root"
-
-case os[:family]
-when "freebsd"
-  user = "nobody"
-  group = "nobody"
-  config = "/usr/local/etc/mosquitto/mosquitto.conf"
-  db_dir = "/var/db/mosquitto"
-  default_group = "wheel"
-when "ubuntu"
-  group = "nogroup"
-when "openbsd"
-  user = "_mosquitto"
-  group = "_mosquitto"
-  db_dir = "/var/db/mosquitto"
-end
+ports = [1883]
 
 describe service(service) do
   it do
-    # Cannot 'status' mosquitto. Set mosquitto_enable to YES in /etc/rc.conf or use 'onestatus' instead of 'status'.
+    # Cannot 'status' mosquitto. Set mosquitto_enable to YES in /etc/rc.conf or
+    # use 'onestatus' instead of 'status'.
     pending "serverspec failes when the service is not enabled" if os[:family] == "freebsd"
     should_not be_running
   end
@@ -56,8 +35,8 @@ else
   end
 end
 
-%w[ mosquitto_sub mosquitto_pub ].each do |c|
-  describe command("#{ c } --help") do
+%w(mosquitto_sub mosquitto_pub).each do |c|
+  describe command("#{c} --help") do
     its(:exit_status) { should eq 1 }
     its(:stderr) { should match(/^$/) }
     its(:stdout) { should match(/^Usage: mosquitto_/) }
